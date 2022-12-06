71f83b5944bc6e11fc7cdde257be8c770f62ea38
Johan Eliasson <johan@eliasson.me>
2022-12-06 21:53:20 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div className="text-gray-300">Hey, I am Johan.</div>
-}
+  return (
+    <div className='text-gray-300'>
+      <p>Hey, I am Johan.</p>
+      <p>
+        Link: <a href='https://vattenfilter.co'>vattenfilter.co</a>
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
